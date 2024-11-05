f345315d8b7fdc322a2d1048a8f8227ed63d3de4
ErickDaniCB <erickdani10@hotmail.com>
2024-11-05 12:10:09 -0600

    initial

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 // index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
-import img from "./space.jpg"
-
-const myImg = new Image();
-myImg.src = img;
-document.body.appendChild(myImg);
